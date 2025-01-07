7e97ad96df0f7fa950577311c3e5b52bb27a2f5f
DEVAlexHenrique <alex.henrique1212@gmail.com>
2025-01-07 18:15:17 -0300

    add: validation

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public  string ErrorMassage { get; set; } = string.Empty;
+
+    public ResponseErrorJson(string errorMessage) 
+    {
+        ErrorMassage = errorMessage;
+    }
+}
