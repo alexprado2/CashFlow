6df7d2b8d651cbec1242c2b48a84581ff1e8f892
DEVAlexHenrique <alex.henrique1212@gmail.com>
2025-02-12 17:20:29 -0300

    add: migration and automatic migration

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using CashFlow.Domain.Entities;
+﻿using CashFlow.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess;
-
 internal class CashFlowDbContext : DbContext
 {
-    public CashFlowDbContext(DbContextOptions options) : base(options) {}
-    public DbSet<Expense> Expenses { get; set; }
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
 
+    public DbSet<Expense> Expenses { get; set; }
 }
